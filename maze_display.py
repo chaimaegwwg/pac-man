92867dcfefb902b4e6fd9ee587d31fa136c53989
htttpmnew-ship-it <htttpmnew@gmail.com>
2026-09-17 10:07:14 +0100

    try to fix the movement of character to be more smoth

--- maze_display.py
+++ maze_display.py
@@ -31,15 +31,6 @@ def display(screen,generator,pos):
             if cell & 8:
                 pygame.draw.line(screen, (100, 80, 120),(x, y),(x, y + cell_size), 3)
 
-            # if pos:
-                # if ((pos[0] == -1) and (cell & 8)) or ((pos[0] == 1) and (cell & 2)):
-                #     print("second -----------> it go ---------------")
-                #     position_pacman[0] =+ pos[0]
-                #     print("=====>this place is go here ")
-                # elif ((pos[1] == -1) and (cell & 1)) or ((pos[1] == 1) and (cell & 4)):
-                #     position_pacman[1] =+ pos[1]
-                    # print("this place is go here ")
-    
             
 #from now i will like just test that and i will start from 0,0 after that i will change it to the middle
 def ft_check_walls(pos, row , col,generator):
@@ -63,6 +54,7 @@ def ft_check_walls(pos, row , col,generator):
 
 screen = pygame.display.set_mode((800,800))
 size_cell = 800 // 15
+start_to = 5
 # pacman = pygame.image.load("pacman_up.png")
 # screen.blit(pacman, (x, y))
 pygame.init()
@@ -70,6 +62,8 @@ pygame.init()
 
 character_pacman = "@"
 position_pacman = [0,0]
+checker_x = 0
+checker_y = 0
 background = (15, 15, 20)
 generator = MazeGenerator(
         size=(15, 15),
@@ -87,6 +81,10 @@ directions = {
     pygame.K_UP: [0,-1], 
     pygame.K_DOWN:[0,1] 
 }
+step_x = 0
+step_y = 0
+x=0
+y=0
 clock = pygame.time.Clock()
 while running:
     for event in pygame.event.get():
@@ -99,14 +97,25 @@ while running:
         row = position_pacman[0]
         col = position_pacman[1]
         if ft_check_walls(pos,row,col,generator):
-            position_pacman[0] += pos[1] 
-            position_pacman[1] += pos[0]
+            step_x += pos[0] * 5.3
+            step_y += pos[1] * 5.3
+            if abs(step_x) >= size_cell:
+                step_x = 0 
+                position_pacman[1] += pos[0]
+            if abs(step_y) >= size_cell:
+                step_y = 0
+                position_pacman[0] += pos[1]
+        x = (position_pacman[1] * size_cell) + step_x
+        y = (position_pacman[0] * size_cell) + step_y
+
+        x += (size_cell - pacman.get_width()) // 2
+        y += (size_cell - pacman.get_height()) // 2
     screen.fill(background)
     display(screen,generator,pos)
     pacman = font.render("@", True, (255, 255, 0))
-    screen.blit(pacman, (position_pacman[1]*size_cell, position_pacman[0]*size_cell))
+    screen.blit(pacman, (x, y))
     pygame.display.flip()
-    clock.tick(5)
+    clock.tick(30)
 
 
 pygame.quit()   
