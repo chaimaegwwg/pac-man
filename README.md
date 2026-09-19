5064d0a69be096cd39e84bda29e35e07c22af637
htttpmnew-ship-it <htttpmnew@gmail.com>
2026-09-19 09:08:43 +0100

    add pac-gum food

--- README.md
+++ README.md
@@ -4,12 +4,31 @@
 --> 2thing 3 lives and the scooring part
 
 now just i fix character to try to move inside the maze but there is the problem on there movement not smoth
+->done
+->i thing now is good but i will return to this part to fix the pixel on each cell and change the wall color and form
+
+
 
 the next mission will:
 
-after that i will do the food 
-and after it extra food and make it count it 
+after that i will do the pacgum 
+normal  pacgum 
+-> done
+do the logic
+-->done
+ 
 
+next mission:
+display:   super-pacgum and make it count it 
+and the logic
+--> done
+
+=>next mission :
+
+replace the @ and pac-gum with the right character
+Ghost
+
+add the try and the exception 
 
 
 
--- maze_display.py
+++ maze_display.py
@@ -1,36 +1,44 @@
 from mazegenerator import MazeGenerator
 import pygame, sys
 
-# top = 1
-# right = 2
-# bottom = 4
-# left = 8
-def display(screen,generator,pos):
+
+def display(screen,generator,pos,food,super_pacgums):
     pygame.init()
     black = (0, 0, 0)
     width, height = screen.get_size()
+    font = pygame.font.Font(None, 40)
     #suppose the arr has 15 col and row that why we will see if it change or not 
     cell_size = min(width//15,height//15)
     for row, line in enumerate(generator.maze):
         for col, cell in enumerate(line):
             x = col * cell_size
             y = row * cell_size
+            pacman = font.render(".", True, (255, 255, 0))
+            super_pacgum = font.render("#",True, (255,255,0))
+            if not (cell &1 and cell & 2 and cell & 4 and cell & 8):
+                if food is None:
+                    screen.blit(pacman, (x+20, y+5))
+                    food = [row.copy() for row in generator.maze]
+                    food[0][0] = 20
+                elif [row,col] in super_pacgums: 
+                    screen.blit(super_pacgum,(x+20,y+5))
+                    if [row,col] == [0,0] and [row,col] in super_pacgums:
+                        super_pacgums.remove([0,0])
+                elif generator.maze[row][col] == food[row][col]:
+                    screen.blit(pacman, (x+20, y+5))
             # top
             if cell & 1:
                 pygame.draw.line(screen, (100, 80, 120),(x, y),(x + cell_size, y), 3)
-
             # right
             if cell & 2:
                 pygame.draw.line(screen, (100, 80, 120),(x + cell_size, y),(x + cell_size, y + cell_size), 3)
-
             # bottom
             if cell & 4:
                 pygame.draw.line(screen, (100, 80, 120),(x, y + cell_size),(x + cell_size, y + cell_size), 3)
-
             # left
             if cell & 8:
                 pygame.draw.line(screen, (100, 80, 120),(x, y),(x, y + cell_size), 3)
-
+    return food,super_pacgums
             
 #from now i will like just test that and i will start from 0,0 after that i will change it to the middle
 def ft_check_walls(pos, row , col,generator):
@@ -54,27 +62,30 @@ def ft_check_walls(pos, row , col,generator):
 
 
 screen = pygame.display.set_mode((800,800))
-size_cell = 800 // 15
+
+width, height = screen.get_size()
+size_cell = min(width//15,height//15)
 start_to = 5
 # pacman = pygame.image.load("pacman_up.png")
 # screen.blit(pacman, (x, y))
 pygame.init()
 pos = None
 pending_pos = None
-#this is for not each time i will press the key
-
 character_pacman = "@"
 position_pacman = [0,0]
 checker_x = 0
 checker_y = 0
 background = (15, 15, 20)
+size=(15, 15)
 generator = MazeGenerator(
-        size=(15, 15),
+        size,
         perfect=False,
         entry_cell=(0, 0),
         exit_cell=(-1, -1),
         seed=0
     )
+
+super_pacgums = [[0,0],[0,size[1]-1],[size[0]-1,0],[size[0]-1,size[1]-1]]
 font = pygame.font.Font(None, 40)
 running = True
 pos= None
@@ -88,6 +99,7 @@ step_x = 0
 step_y = 0
 x=0
 y=0
+food = None
 clock = pygame.time.Clock()
 while running:
     for event in pygame.event.get():
@@ -99,29 +111,38 @@ while running:
     if pending_pos is not None and step_x == 0 and step_y == 0:
             pos = pending_pos
             pending_pos = None
+
     if pos:
         row = position_pacman[0]
         col = position_pacman[1]
         if ft_check_walls(pos,row,col,generator):
-            step_x += pos[1] * 5
-            step_y += pos[0] * 5
+            step_x += pos[1] * 3
+            step_y += pos[0] * 3
             if abs(step_x) >= size_cell:
                 step_x = 0
                 position_pacman[1] += pos[1]
+                if food:
+                    food[row][position_pacman[1]] = 20
+                    if [row,position_pacman[1]] in super_pacgums:
+                        super_pacgums.remove([row,position_pacman[1]])
               
             if abs(step_y) >= size_cell:
                 step_y = 0
                 position_pacman[0] += pos[0]
-                
+                if food:
+                    food[position_pacman[0]][col] = 20
+                    if [position_pacman[0],col] in super_pacgums:
+                        print("iiiii eaat it also ",position_pacman[0],col)
+                        super_pacgums.remove([position_pacman[0],col])
     x = (position_pacman[1] * size_cell) + step_x
     y = (position_pacman[0] * size_cell) + step_y
 
     screen.fill(background)
-    display(screen,generator,pos)
+    food,super_pacgums = display(screen,generator,pos,food,super_pacgums)
     pacman = font.render("@", True, (255, 255, 0))
-    screen.blit(pacman, (x+10, y+5))
+    screen.blit(pacman, (x+10, y+10))
     pygame.display.flip()
-    clock.tick(30)
+    clock.tick(60)
 
 
 pygame.quit()   
